274d731b0e7ba5126f234b4b581d4b31013f5696
MichelHT <61551024+MichelHT@users.noreply.github.com>
2020-04-17 17:15:33 +0200

    Add files via upload

--- Study/Exterior_Characteristic.m
+++ Study/Exterior_Characteristic.m
@@ -4,8 +4,8 @@ function Exterior_Characteristic
 
 %% Directory containing the simulations results
 
-% Directory = 'Core_type_first_try';
-Directory = 'Shell_type_first_try';
+Directory = 'Core_type_first_try';
+% Directory = 'Shell_type_first_try';
 
 
 %% Importation of the data and computation of the norm of the secondary current and voltage
@@ -53,4 +53,20 @@ xlabel('$I_2$ [A]');
 grid;
 legend('Phase 1', 'Phase 2', 'Phase 3');
 set(gca,'fontsize',20,'fontname','Times','LineWidth',0.5);
+
+
+%% Fresnel diagram open circuit
+Figure2=figure(2);clf;set(Figure2,'defaulttextinterpreter','latex');
+col = {'r' 'b' 'k'}
+hold on;
+grid;
+for(i=1:3)    
+Vec_V2_Vide{i} = [V2{i}(end,2) V2{i}(end,3)];
+line([0 Vec_V2_Vide{i}(1)] , [0 Vec_V2_Vide{i}(2)],'linewidth',3,'color',col{i});
+end
+axis('equal')
+legend('Phase 1', 'Phase 2', 'Phase 3');
+set(gca,'fontsize',20,'fontname','Times','LineWidth',0.5);
+
+
 end
